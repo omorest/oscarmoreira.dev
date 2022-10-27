9bc369f51daaef29639f4471d19391732cf5fdf6
omorest <oscar.moreira.estevez@gmail.com>
2022-10-27 18:16:21 +0100

    sigueme footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,9 @@ interface FooterProps {
 const Footer: FC<FooterProps> = () => {
   return (
     <div className='flex flex-col items-center justify-around h-[200px] m-10 text-black'>
-      <h2 className='text-3xl font-semibold'>Follow me</h2>
+      <h2 className='text-3xl font-semibold'>Sígueme</h2>
       <Icons />
-      <span>Made by Oscar Moreira</span>
+      <span>Hecho por Oscar Moreira</span>
     </div>
   )
 }
