c5180f6503f8010766b68fb696ba84d99d4cf8f8
omorest <oscar.moreira.estevez@gmail.com>
2022-10-27 21:04:34 +0100

    dark mode

--- components/Description.tsx
+++ components/Description.tsx
@@ -9,12 +9,12 @@ interface DescriptionProps {
 const Description:FC<DescriptionProps> = ({ title, titleColored, description }) => {
   return (
     <div className="mt-32">
-      <h2 className="text-6xl font-semibold text-black">{title}&nbsp;
+      <h2 className="text-6xl font-semibold text-black dark:text-white">{title}&nbsp;
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-hard via-purple-medium to-purple-soft">
           {titleColored}
         </span>
       </h2>
-      <p className="mt-8 text-gray text-xl font-light">
+      <p className="mt-8 text-gray text-xl font-light dark:text-white">
         {description}
       </p>
     </div>
