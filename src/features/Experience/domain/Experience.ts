a30eec7933d756881bf9ae28b5a012541d63e857
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-19 22:15:04 +0000

    feat: todo coment

--- src/features/Experience/domain/Experience.ts
+++ src/features/Experience/domain/Experience.ts
@@ -3,8 +3,8 @@ type Languages = "Spanish" | "English"
 export interface Experience {
   company: string;
   position: string;
-  startDate: string;
-  endDate: string;
+  startDate: string; // TODO: change to date and create a formater to put the date in the correct format like may 2020 - june 2021
+  endDate: string; // TODO: change to date and create a formater to put the date in the correct format like may 2020 - june 2021
   technologies: string[];
   languaje: Languages[];
   content?: string;
