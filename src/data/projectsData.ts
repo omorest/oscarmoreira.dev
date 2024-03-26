1253bbaaa76ed6526e48fcec441a099851deb463
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2024-03-26 15:54:03 +0000

    projects

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,11 +1,13 @@
 import type { Project } from "../models/Project";
 
+
 export const projectsData: Project[] = [
   {
     name: "GitHub Wrapped",
     authors: ["Óscar Moreira"],
-    description: "A GitHub profile summary generator",
-    linkApp: "https://github-wrapped.vercel.app/",
+    description: "A GitHub profile summary generator. Your last followers, repositories, and more in a single page.",
+    image: "src/data/images/gh-wrapped.png",
+    linkApp: "https://gh-wrapped.vercel.app/",
     linkProject: "https://github.com/omorest/Github-Wrapped",
     publishDate: "2024",
     technologies: ["NextJS", "React", "TypeScript", "Vercel"]
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -3,6 +3,7 @@ import type { Technologies } from "./Technologies"
 export type Project = {
   name: string
   description: string
+  image: string
   technologies: Technologies[]
   authors: string[]
   linkApp: string
